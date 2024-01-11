fd1ed6c4b0298af1a18b11b7b0416205f68541b2
wjlee611 <wjlee611@gmail.com>
2024-01-11 21:12:56 +0900

    implement diary skeleton

--- lib/pages/home/screens/diary/diary_screen.dart
+++ lib/pages/home/screens/diary/diary_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:unknown_note_flutter/constants/sizes.dart';
 import 'package:unknown_note_flutter/package/flutter_card_swiper/card_swiper.dart';
-import 'package:unknown_note_flutter/pages/home/screens/diary/widgets/diary_card.dart';
+import 'package:unknown_note_flutter/pages/home/screens/diary/widgets/diary_skeleton.dart';
 
 class DiaryScreen extends StatefulWidget {
   const DiaryScreen({super.key});
@@ -28,17 +28,21 @@ class _DiaryScreenState extends State<DiaryScreen> {
   @override
   Widget build(BuildContext context) {
     return CardSwiper(
-      cardsCount: candidates.length,
-      threshold: 200,
+      cardsCount: 3,
+      numberOfCardsDisplayed: 3,
+      threshold: 150,
+      backCardOffset: const Offset(0, 50),
       padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).padding.bottom + Sizes.size60,
+        bottom: MediaQuery.of(context).padding.bottom + Sizes.size72,
+        top: Sizes.size20,
+        left: Sizes.size20,
+        right: Sizes.size20,
       ),
-      cardBuilder: (context, index, _, __) => DiaryCard(
-        candidate: candidates[index],
+      cardBuilder: (context, index, _, __) => DiarySkeleton(
+        seed: index,
       ),
       onSwipe: (previousIndex, currentIndex, direction) {
-        _controller.undo();
-        return false;
+        return true;
       },
     );
   }
---
+++ lib/pages/home/screens/diary/widgets/diary_skeleton.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:unknown_note_flutter/common/widgets/common_shadow_container.dart';
+import 'package:unknown_note_flutter/constants/gaps.dart';
+import 'package:unknown_note_flutter/constants/sizes.dart';
+
+class DiarySkeleton extends StatelessWidget {
+  final int seed;
+
+  const DiarySkeleton({
+    super.key,
+    required this.seed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonShadowContainer(
+      child: Padding(
+        padding: const EdgeInsets.all(Sizes.size20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  Container(
+                    width: Sizes.size64,
+                    height: Sizes.size64,
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  Gaps.v10,
+                  _rowSkeleton(width: 100),
+                  _rowSkeleton(width: 50),
+                  Gaps.v20,
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (var w in List.generate(
+                    Random(seed).nextInt(10) + 5,
+                    (index) =>
+                        Random(seed * 100 + index).nextDouble() *
+                            (MediaQuery.of(context).size.width - 200) +
+                        100))
+                  _rowSkeleton(width: w),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _rowSkeleton({
+    double width = double.infinity,
+  }) =>
+      Container(
+        margin: const EdgeInsets.only(bottom: Sizes.size10),
+        width: width,
+        height: Sizes.size10,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(Sizes.size10),
+        ),
+      );
+}
